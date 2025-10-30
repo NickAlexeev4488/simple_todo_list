b586eb48de79d659e23f2e9778aa39635ba09649
Nikolai Alexeev <N.Alexeev@seventest.ru>
2025-10-30 23:52:24 +0300

    created addNewCard button

--- code.js
+++ code.js
@@ -18,9 +18,27 @@ searchInput.classList.add('searchInput')
 searchInput.placeholder = 'Поиск по названию'
 
 
-// Добавляем дочерние теги
+// Добавляем дочерние теги header
 logo.appendChild(logoText)
 searchDiv.appendChild(searchInput)
 header.append(logo, searchDiv)
 
+// Основной блок
+let main = document.createElement('section')
+main.classList.add('mainBlock')
+
+// Блок добавления карточки
+let newCardButton = document.createElement('button')
+newCardButton.classList.add('newCardButton')
+newCardButton.innerText = 'Новая запись'
+
+// Контейнер карточек
+let cardContainer = document.createElement('div')
+cardContainer.classList.add('cardContainer')
+
+// Добавляем дочерние теги main
+main.appendChild(newCardButton)
+main.appendChild(cardContainer)
+
 body.appendChild(header)
+body.appendChild(main)
